842868dede2b62a33f747920eb0762a119231ef4
Kasper <kasperbak89@pm.me>
2023-05-25 21:31:09 +0200

    Refactor convert to unicode to make it more useful

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -3,9 +3,16 @@
 
 
 class CaesarCipher
+  LOWERCASE_RANGE = [97..122]
+  UPERCASE_RANGE = [65..90]
 
-  def convert_to_unicode(letter)
-    letter.ord
+  def convert_to_unicode(message)
+    splitted_message = message.split("")
+    splitted_message.map { |v| v.ord }
+  end
+
+  def shift_letters(unicode_array) do
+    unicode_array.map { |v| v.between()}
   end
 
   def caesar_cipher(message, shift_factor)
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -6,24 +6,45 @@ describe CaesarCipher do
   subject(:cipher) { described_class.new}
 
   describe 'convert_to_unicode' do
-    context 'when passed a lower case letter' do
-      context 'when passed d' do
-        it 'returns 100' do
-          lower_case_letter = 'd'
-          result = cipher.convert_to_unicode(lower_case_letter)
-          expect(result).to eq(100)
+    context 'when passed a simple 3 letter message' do
+      context 'when passed hot' do
+        it 'returns an array of unicode values [104, 111, 116]' do
+          message = 'hot'
+          result = cipher.convert_to_unicode(message)
+          expect(result).to eq([104, 111, 116])
         end
       end
     end
 
-    context 'when passed an upper case letter' do
-      context 'when passed D' do
-        it 'returns 68' do
-          letter = 'D'
-          result = cipher.convert_to_unicode(letter)
-          expect(result).to eq(68)
+    context 'when passed an upper case simple message' do
+      context 'when passed HOT' do
+        it 'returns [72, 79, 84]' do
+          message = 'HOT'
+          result = cipher.convert_to_unicode(message)
+          expect(result).to eq([72, 79, 84])
         end
       end
+    end
+
+    context 'when passed a diverse message' do
+      context 'when passed Hello World!' do
+        it 'returns [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]' do
+          message = 'Hello World!'
+          result = cipher.convert_to_unicode(message)
+          expectation = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
+          expect(result).to eq(expectation)
+        end
+      end
+    end
+  end
+
+  describe '#shift_letters' do
+    context 'when passing [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33] with 3 as a shift factor'
+    it 'returns [75, 104, 111, 111, 32, 90, 114, 117, 111, 103, 33]' do
+      unicode_array = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
+      result = cipher.shift_letters(unicode_array)
+      expectation = [75, 104, 111, 111, 32, 90, 114, 117, 111, 103, 33]
+      expect(result).to eq(expectation)
     end
   end
 
